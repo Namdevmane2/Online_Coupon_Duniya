d66ba56e6b34855f3aba86d4b776406fb035a30b
Namdevmane2 <manenamdev2@gmail.com>
2021-07-30 02:40:44 +0530

    yes

--- searchquery.js
+++ searchquery.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-$('#search').keyup(function(){
-    $('#result').html('');
-    var searchfield = $('#search').val();
-    var expression = new RegExp(searchfield,"i");
-    $.getJSON('data.json',function(data){
-        $.each(data,function(key,value){
-            if(value.name.search(expression) != -1){
-                $('#result').append('<li class ="list-group-item'>value.name);
-            }
-        });
-    })
+$(document).ready(function () {
+  $("#search").keyup(function () {
+    $("#result").html("");
+    var searchfield = $("#search").val();
+    var expression = new RegExp(searchfield, "i");
+    $.getJSON("data.json", function (data) {
+      $.each(data, function (key, value) {
+        if (value.name.search(expression) != -1) {
+          $("#result").append('<li class ="list-group-item' > value.name);
+        }
+      });
+    });
+  });
 });
-}
